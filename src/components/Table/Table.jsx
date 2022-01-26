acf2f11fbbb869e13e244f7a159f826025b5f6b1
Anton <chiga2030@gmail.com>
2022-01-26 19:23:22 +0300

    create a table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,8 +1,104 @@
-const Table = () => (
-  <>
-    Table
-  </>
+import styles from './Table.module.css';
+
+
+const Table = ({
+  date = '21.11.2021',
+  classesList,
+  studentsPerformance,
+}) => {
+  console.log();
+
+  return (
+    <div className={ styles.wrapper }>
+      <Header date={ date } classesList={ classesList } />
+      { studentsPerformance.students.map((student, index) => (
+        <TableBody
+          date={ date }
+          classesName={ classesList.classes.map(classes => classes.name) }
+          student={ student }
+          key={ String(index + Date.now()) }
+        />
+      )) }
+    </div>
+  );
+};
+
+
+const Header = ({
+  date,
+  classesList,
+}) => (
+  <header className={ styles.header }>
+    <div className={ `
+      ${styles.headerCell}
+      ${styles.studentName}
+    ` }>
+      ФИО студента
+    </div>
+    { classesList.classes
+      .filter(study => study.date === date)
+      .map(stydy => (
+        <HeaderCell
+          studyName={ stydy.name }
+          date={ date }
+          key={ stydy.name }
+        />)) }
+  </header>
 );
+
+const HeaderCell = ({
+  studyName,
+  date,
+}) => (
+  <div className={ `
+    ${styles.headerCell}
+    ${styles.study}
+  ` }>
+    <span className={ styles.studyName }>{ studyName }</span>
+    <span>{ date }</span>
+  </div>
+);
+
+
+const TableBody = ({
+  date,
+  classesName,
+  student,
+}) => (
+  <div className={ styles.tableBody }>
+    <div className={ `${styles.bodyCell} ${styles.studentName}` }>
+      { student.name }
+    </div>
+    <BodyCell
+      date={ date }
+      classesName={classesName}
+      studentPerformance={ student.performance }
+    />
+  </div>
+);
+
+const BodyCell = ({
+  date,
+  classesName,
+  studentPerformance,
+}) => {
+  console.log(classesName);
+  console.log('here - ', studentPerformance);
+  return (
+    <>
+      { studentPerformance.classes
+        .filter((study, index) => (study.date === date
+          && study.name === classesName[index]))
+        .map((study, index) => (
+          <div
+            className={ `${styles.bodyCell} ${styles.study}` }
+            key={ String(index + Date.now()) }
+          >
+            { study.score }
+          </div>)) }
+    </>
+  );
+};
 
 
 export default Table;

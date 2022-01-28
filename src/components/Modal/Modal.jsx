dbe0c8c91b6c5511f11b2bce3fe7a2b322e6d32e
Anton <chiga2030@gmail.com>
2022-01-28 11:53:18 +0300

    component Modal rebased to separate component

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,142 @@
+import styles from './Modal.module.css';
+
+import {
+  useState,
+  useEffect,
+} from 'react';
+
+import {
+  ReactComponent as NotMarkCheckboxImg,
+} from './images/notMarkCheckbox.svg';
+import {
+  ReactComponent as MarkCheckboxImg,
+} from './images/markCheckbox.svg';
+import {
+  ReactComponent as TailImg,
+} from './images/tail.svg';
+
+
+const Modal = ({
+  coordinates,
+  currentUser,
+  isShow,
+  onShowToggle,
+}) => {
+  const [
+    checkboxState,
+    setCheckboxState,
+  ] = useState(false);
+  const [
+    scoreState,
+    setScoreState,
+  ] = useState(' ');
+
+  useEffect(() => {
+    setScoreState(currentUser.score);
+  }, [
+    currentUser.score,
+  ]);
+
+  const onChangeInputHandler = event => {
+    const newValue = () => {
+      if (event.target.value.match(/^[1-5]$/)) {
+        return event.target.value;
+      } return 'Ошибка: нужно указать оценку от 1 до 5';
+    };
+    return setScoreState(newValue);
+  };
+
+  return (
+    <>
+      <div
+        className={ `
+          ${styles.modalDarkLayer}
+          ${isShow ? false : styles.modalDarkLayerHidden}` }
+        onPointerUp={ () => onShowToggle(!isShow) }
+      >
+      </div>
+      <div
+        id="modal"
+        className={ `
+          ${styles.modal}
+          ${styles[`modalTransform${coordinates.side}`]}
+          ${isShow ? false : styles.modalHidden}
+        ` }
+        style={ coordinates.modal }
+      >
+        <TailImg
+          className={ `
+            ${styles.modalTail}
+            ${styles[`modalTail${coordinates.side}`]}
+          ` }
+        />
+        <header>
+          <h3 className={ styles.modalHeader }>Поставить отметку</h3>
+        </header>
+        <main>
+          <p className={ styles.modalParagraphs }>
+            <span>Студент</span>
+            <span className={ styles.modalUsername }>{ currentUser.name }</span>
+          </p>
+          <p className={ styles.modalParagraphs }>
+            <span>Дата</span>
+            <span className={ styles.modalDate }>{ currentUser.date }</span>
+          </p>
+          <form id="studentAccountingForm">
+            <input
+              type="hidden"
+              name="studentName"
+              value={ currentUser.name ? currentUser.name : false }
+            />
+
+            <input
+              type="hidden"
+              name="date"
+              value={ currentUser.date ? currentUser.date : false }
+            />
+
+            <input
+              type="checkbox"
+              name="isNotAttendance"
+              id="checkAttendance"
+              className={ styles.modalCheckbox }
+              checked={ checkboxState }
+              onChange={ () => setCheckboxState(!checkboxState) }
+            />
+            <label
+              className={ styles.modalLabel }
+              htmlFor="checkAttendance"
+            >
+              { checkboxState ?
+                <MarkCheckboxImg className={ styles.modalCustomCheckbox } /> :
+                <NotMarkCheckboxImg
+                  className={ styles.modalCustomCheckbox } /> }
+              Не присутствовал
+            </label>
+
+            <input
+              className={ styles.modalInputScore }
+              type="text"
+              value={ scoreState ? scoreState : ' ' }
+              onChange={ event => onChangeInputHandler(event) }
+              onClick={ event => event.target.select() }
+              onKeyUp={ event => event.target.select() }
+            />
+
+            <div className={ styles.modalButtonWrapper }>
+              <button
+                className={ styles.modalButton }
+                type="button"
+              >
+                Поставить отметку
+              </button>
+            </div>
+          </form>
+        </main>
+      </div>
+    </>
+  );
+};
+
+
+export default Modal;
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,21 +2,12 @@ import styles from './Table.module.css';
 
 import {
   useState,
-  useEffect,
 } from 'react';
+
+import Modal from '../Modal/Modal';
 
 import Header from './Header';
 import TableBody from './TableBody';
-
-import {
-  ReactComponent as NotMarkCheckboxImg,
-} from './images/notMarkCheckbox.svg';
-import {
-  ReactComponent as MarkCheckboxImg,
-} from './images/markCheckbox.svg';
-import {
-  ReactComponent as TailImg,
-} from './images/tail.svg';
 
 const Table = ({
   date = '21.11.2021',
@@ -149,129 +140,6 @@ const Table = ({
         onGetElementPlaceAndUserData={ onGetElementPlaceAndUserData }
       />
     </div>
-  );
-};
-
-
-const Modal = ({
-  coordinates,
-  currentUser,
-  isShow,
-  onShowToggle,
-}) => {
-  const [
-    checkboxState,
-    setCheckboxState,
-  ] = useState(false);
-  const [
-    scoreState,
-    setScoreState,
-  ] = useState(' ');
-
-  useEffect(() => {
-    setScoreState(currentUser.score);
-  }, [
-    currentUser.score,
-  ]);
-
-  const onChangeInputHandler = event => {
-    const newValue = () => {
-      if (event.target.value.match(/^[1-5]$/)) {
-        return event.target.value;
-      } return 'Ошибка: нужно указать оценку от 1 до 5';
-    };
-    return setScoreState(newValue);
-  };
-
-  return (
-    <>
-      <div
-        className={ `
-          ${styles.modalDarkLayer}
-          ${isShow ? false : styles.modalDarkLayerHidden}` }
-        onPointerUp={ () => onShowToggle(!isShow) }
-      >
-      </div>
-      <div
-        id="modal"
-        className={ `
-          ${styles.modal}
-          ${styles[`modalTransform${coordinates.side}`]}
-          ${isShow ? false : styles.modalHidden}
-        ` }
-        style={ coordinates.modal }
-      >
-        <TailImg
-          className={ `
-            ${styles.modalTail}
-            ${styles[`modalTail${coordinates.side}`]}
-          ` }
-        />
-        <header>
-          <h3 className={ styles.modalHeader }>Поставить отметку</h3>
-        </header>
-        <main>
-          <p className={ styles.modalParagraphs }>
-            <span>Студент</span>
-            <span className={ styles.modalUsername }>{ currentUser.name }</span>
-          </p>
-          <p className={ styles.modalParagraphs }>
-            <span>Дата</span>
-            <span className={ styles.modalDate }>{ currentUser.date }</span>
-          </p>
-          <form id="studentAccountingForm">
-            <input
-              type="hidden"
-              name="studentName"
-              value={ currentUser.name ? currentUser.name : false }
-            />
-
-            <input
-              type="hidden"
-              name="date"
-              value={ currentUser.date ? currentUser.date : false }
-            />
-
-            <input
-              type="checkbox"
-              name="isNotAttendance"
-              id="checkAttendance"
-              className={ styles.modalCheckbox }
-              checked={ checkboxState }
-              onChange={ () => setCheckboxState(!checkboxState) }
-            />
-            <label
-              className={ styles.modalLabel }
-              htmlFor="checkAttendance"
-            >
-              { checkboxState ?
-                <MarkCheckboxImg className={ styles.modalCustomCheckbox } /> :
-                <NotMarkCheckboxImg
-                  className={ styles.modalCustomCheckbox } /> }
-              Не присутствовал
-            </label>
-
-            <input
-              className={ styles.modalInputScore }
-              type="text"
-              value={ scoreState ? scoreState : ' ' }
-              onChange={ event => onChangeInputHandler(event) }
-              onClick={ event => event.target.select() }
-              onKeyUp={ event => event.target.select() }
-            />
-
-            <div className={ styles.modalButtonWrapper }>
-              <button
-                className={ styles.modalButton }
-                type="button"
-              >
-                Поставить отметку
-              </button>
-            </div>
-          </form>
-        </main>
-      </div>
-    </>
   );
 };
 
